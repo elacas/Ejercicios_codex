fdb29ab4dcf785e349c075f0b82b7193584807ec
elacas <eli_dkf_@hotmail.com>
2025-03-09 20:53:23 +0100

    Ejercicios

---
+++ Teorema_de_Pitagoras.py
@@ -0,0 +1,22 @@
+#username= input("Enter your name ")
+import math
+
+#print(username)
+
+#age= int(input("what's your age?"))
+
+#print(age)
+
+#Teorema de Pitágoras
+
+#c**2= a**2+b**
+
+a=5**2
+
+b=3**2
+
+d=b+a
+
+print(d)
+math.sqrt(d)
+
