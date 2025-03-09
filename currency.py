68630f65745424d95af1c860e68a164a1f10bb2c
elacas <eli_dkf_@hotmail.com>
2025-03-09 21:18:21 +0100

    ¡¡Me salió!! :D

---
+++ currency.py
@@ -0,0 +1,13 @@
+#co=Colombian pesos 0.00022-1Euro
+#pe=peruvian soles 0.25-1Euro
+#br=brasilian reais 0.15-1 euro
+
+co=int(input("¿Cuántos pesos colombianos has gastado?"))
+pe=int(input("¿Cuántos soles peruanos has gastado?"))
+br=int(input("Cuántos reales brasileños has gastado?"))
+
+print(co*0.00022)
+print(pe*0.25)
+print(br*0.15)
+
+
