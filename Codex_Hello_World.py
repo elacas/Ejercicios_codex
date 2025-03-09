4ff06efc620718f4043fe426b181d4b738210489
elacas <eli_dkf_@hotmail.com>
2025-03-09 19:09:10 +0100

    Ejercicios Codex Python Básico

---
+++ Codex_Hello_World.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+print('🌻 Morning Dharma!')
+print('🙋🏽 Evening Sonny!')
+
+#Patterns
+
+print("    1")
+print("   23")
+print("  456")
+print("78910")
---
+++ Initials.py
@@ -0,0 +1,8 @@
+#Yo soy un lato, una tetera, una cuchara, un cucharon
+print("EEEEEE    CCCCCC")
+print("E        C")
+print("E       C")
+print("EEEE    C")
+print("E       C")
+print("E        C")
+print("EEEEEE    CCCCCC")
