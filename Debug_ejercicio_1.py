e01f329649b5571145dfb3faa14ed7a050ff1a96
elacas <eli_dkf_@hotmail.com>
2025-03-09 22:02:00 +0100

    Ejercicio de debug #1

---
+++ Debug_ejercicio_1.py
@@ -0,0 +1,21 @@
+#butterflies = 10
+#beetles = 12
+#ladybugs = 20
+
+#print('I caught ' + butterflies + ' 🦋 butterflies!')
+#print('I caught ' + beetles + ' 🪲 beetles!')
+#print('I caught ' + ladybug + ' 🐞 ladybugs!)
+
+#print('I caught ' + str(total) + ' total bugs!'
+#------------------------------------------------------------------------------------------------------
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+print('I caught '+str(butterflies)+' 🦋 butterflies!')
+print('I caught '+str(beetles)+' 🪲 beetles!')
+print('I caught '+str(ladybugs)+' 🐞 ladybugs!')
+
+total= butterflies+beetles+ladybugs
+
+print('I caught ' + str(total) + ' total bugs!')
